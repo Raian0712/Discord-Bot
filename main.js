4e214b80859dbbc3096315de081ae334b170b343
Raian0712 <55100222+Raian0712@users.noreply.github.com>
2020-04-04 14:09:38 +0800

    eslint and fixing eval

--- main.js
+++ main.js
@@ -1,53 +1,59 @@
-//Discord API
-"use strict";
+/* eslint-disable linebreak-style */
+/* eslint-disable max-len */
+// eslint-disable-next-line linebreak-style
+// Discord API
+'use strict';
 
 const Discord = require('discord.js');
-const { prefix, ownerID } = require('./config.json');
+const {prefix, ownerID} = require('./config.json');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', message=> {
-    console.log(message.content);
-    const args = message.content.split(" ").slice(1);
+client.on('message', (message)=> {
+  console.log(message.content);
+  const args = message.content.split(' ').slice(1);
 
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    if (message.content.startsWith(prefix + 'ping')) {
-        message.channel.send('\`\`\`pong\`\`\`');
+  if (message.content.startsWith(prefix + 'ping')) {
+    message.channel.send('\`\`\`pong\`\`\`');
+  } else if (message.content.startsWith(prefix + 'user')) {
+    message.channel.send(`\`\`\`Your user name: ${message.author.username}\nYour ID: ${message.author.id}\`\`\``);
+  } else if (message.content.startsWith(prefix + 'close')) {
+    message.channel.send(`\`\`\`Closing..\`\`\``);
+    process.exit();
+  } else if (message.content.startsWith(prefix + 'code')) {
+    if (message.author.id !== ownerID) return;
+    try {
+      const code = args.join(' ');
+      console.log('code = ' + code);
+      let evaled = eval(code);
+
+      if (typeof evaled !== 'string') {
+        evaled = require('util').inspect(evaled);
+      }
+
+      message.channel.send(clean(evaled), {code: 'xl'});
+    } catch (error) {
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(error)}\n\`\`\``);
     }
-    else if(message.content.startsWith(prefix + 'user')) {
-        message.channel.send(`\`\`\`Your user name: ${message.author.username}\nYour ID: ${message.author.id}\`\`\``);
-    }
-    else if(message.content.startsWith(prefix + 'close')) {
-        message.channel.send(`\`\`\`Closing..\`\`\``);
-        process.exit();
-    }
-    else if(message.content.startsWith(prefix + 'code')) {
-        if (message.author.id !== ownerID) return;
-        try {
-            const code = args.join(" ");
-            console.log("code = " + code);
-            let evaled = eval(code);
-
-            if(typeof evaled !== "string") {
-                evaled = require("util").inspect(evaled);
-            }
-
-            message.channel.send(clean(evaled), {code: "xl"});
-        } catch (error) {
-            message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(error)}\n\`\`\``)
-        }
-    }
-});
-
-function clean(text) {
-    if (typeof(text) === "string")
-        return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-    else
-        return text;
   }
+});
+
+/**
+ * cleans text to code
+ * @param {string} text
+ * @return {string} output code
+ */
+function clean(text) {
+  if (typeof(text) === 'string') {
+    return text.replace(/`/g, '`' + String.fromCharCode(8203)).replace(/@/g, '@' + String.fromCharCode(8203));
+  } else {
+    return text;
+  }
+}
 
 client.login(process.env.TOKEN);
